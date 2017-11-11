039bbee4b3d449d4efd16b3f35e0781330bc9243
pauliantonia <paulinaantonia.94@gmail.com>
2017-11-11 00:17:04 -0300

    actualizando cambios de twitter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,46 @@
 window.onload = function() { /*sirve para que cuando cargue la página, cargue todo lo demás*/
-    var sendButton = document.getElementsByName("enviar")[0];/*cada vez que es con class se pone un 0, porque le entrego una posicion que es 0, por defecto es la primera*/
+    var botonTweet = document.getElementsByName("enviar")[0];/*cada vez que es con class se pone un 0, porque le entrego una posicion que es 0, por defecto es la primera*/
     var thinkInput = document.getElementsByName("think")[0];
-    var comentaries = document.getElementsByName("comments")[0];
+    var contPadre = document.getElementsByName("contPadre")[0];
 
-    thinkInput.onkeydown = function(e){
-        console.log(e)
-        var total_caracter = 140;
-        var carcateres = thinkInput.value.length;//.value me rescata lo que escribe el usuario
-        var caracter_restante = total_caracter - carcateres;
-        if(carcateres >= total_caracter && e.key != 'Backspace'){
-            return false;
-        } else {
-             document.getElementById('contador_text').innerHTML = caracter_restante
-        }
-     }
-
-    sendButton.onclick = function() {
+  botonTweet.onclick = function() { //llamo a la funcion onclick, cuando hago click al boton
         if (thinkInput.value == "") {
-            sendButton == null;
+            alert ("Ingrese texto");
         } else {
-            var twitDiv = document.createElement("div");
-            var dateSpan = document.createElement("span");
-            var nameSpan = document.createElement("span");
-            var twitP = document.createElement("p");
-            twitDiv.appendChild(nameSpan);
-            twitDiv.appendChild(dateSpan);
-            twitDiv.appendChild(twitP);
-            twitDiv.className = "twit";
-            dateSpan.textContent = new Date;
-            nameSpan.textContent = "Paulina dice ";
-            twitP.textContent = thinkInput.value;
-            
-            comentaries.insertBefore(twitDiv, comentaries.children[0]);
+            var twitDiv = document.createElement("div"); //creo elemento div vacio
+            var dateSpan = document.createElement("span"); //creo span donde ira la fecha
+            var nameSpan = document.createElement("span"); // creo span donde ira el nombre del usuario
+            var twitP = document.createElement("p"); //creo un parrafo
+            twitDiv.appendChild(nameSpan); // digo que div creado (twitDiv) es padre de nameSpan
+            twitDiv.appendChild(dateSpan); // digo que div creado (twitDiv) es padre de dateSpan
+            twitDiv.appendChild(twitP); // digo que div creado (twitDiv) es padre de twitP
+            twitDiv.className = "tweet"; // le doy nombre a mi twitDiv para llamarlo despues en css
+            dateSpan.textContent = new Date; // digo que contenido de texto tendra = la hora exacta actualizada por cada tweet que se haga.
+            nameSpan.textContent = "Paulina dice "; // dugo que contenido de texto tendra el nombre de usuario y dice. para simular que lo que escribe lo dice el usuario.
+            twitP.textContent = thinkInput.value; // digo que el contenido de twitP sera el valor(contenido) en thinkinpu o lo que se escriba en el textarea. osea lo que se escriba le digo que sera un parrafo.
+
+            contPadre.insertBefore(twitDiv, contPadre.children[0]); // digo que lo ultimo que se escriba quede arriba.
         }
     }
+
+    thinkInput.onkeydown = function() {
+          var maxCaracteres = 140;
+          var cantidad1 = 120;
+          var cantidad2 = 130;
+          var textIngresado = thinkInput.value;
+          var contador = document.getElementsByName("contadorTwit")[0];
+          var largo = textIngresado.length;
+          contador.innerHTML = maxCaracteres - largo;
+          if (largo > maxCaracteres) {
+            botonTweet.disabled = true;
+          } else if (largo < maxCaracteres) {
+            botonTweet.disabled = false;
+          } else if (largo >= maxCaracteres) {
+            contador.style.color="red";
+          }
+        }
+
+
+
 
 }
